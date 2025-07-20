56e750f58714b6b28c6ccd6368f1e22a346605bf
noash0040 <noashahaf4@gmail.com>
2025-07-20 07:38:20 -0700

    added url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/tash/",
   plugins: [react(), VitePWA({
     registerType: 'prompt',
     injectRegister: false,
